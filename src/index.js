741281c65b24a64487e940d52e1ef62572a57803
Yomaira Escano <yjescano@gmail.com>
2019-03-30 05:41:45 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React, { Component } from 'react' // We need to import react so we can make use of its .component class
-// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -10,30 +10,52 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends Component {
-
-  // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
+  render() {
+    return (
+      <div class="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export class InFrontOfYou extends Component {
-  // your code here
+  render() {
+    return (
+      <div>
+        <p>You shouldn't look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
 
 export class ButcherShop extends Component {
-  // your code here
+  render() {
+    const productList = BUTCHER_PRODUCTS.map(product => <li>{product}</li>)
+    return (
+      <div class="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          {productList}
+        </ul>
+      </div>
+    )
+  }
 }
-
 
 export class App extends Component {
   render() {
     return (
       <div id="app">
-
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
       </div>
     )
   }
